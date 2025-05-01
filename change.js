4e48c991286ed90b7b211150944d2555b92c6f12
Sam Fabio <samjamalcode@gmail.com>
2025-05-01 21:18:24 +0200

    Add another function

--- change.js
+++ change.js
@@ -4,3 +4,13 @@ function add(a, b) {
     return a * b;
 }
 console.log("Adding miscellaneous feature");
+
+let mySet = new Set(["sam", "john", "ali"]);
+let myArray = [];
+
+for (let value of mySet) {
+    return push.mySet(value);
+}
+for (let i = myArray.length -1; i = myArray.length; i --) {
+    return myArray[i];
+}
